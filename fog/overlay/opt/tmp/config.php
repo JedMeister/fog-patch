c09f582792eb4af5b479c93af850a8d83e9f1050
Jeremy Davis <jedmeister@turnkeylinux.org>
2011-11-15 11:20:10 +0000

    clean up

--- fog/overlay/opt/tmp/config.php
+++ fog/overlay/opt/tmp/config.php
@@ -16,7 +16,7 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *   config.php location: /var/www/fog/comm
+ *   config.php location: /var/www/fog/commons
  */
 
 define( "IS_INCLUDED", true );
